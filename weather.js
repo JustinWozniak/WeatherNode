4f3f2e105ac29f69ac54cb2b42ecad26eac6353f
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-19 13:30:54 -0400

    Got 5-day weather to display. Currently working on day/night icons

--- weather.js
+++ weather.js
@@ -1,46 +1,70 @@
+let key = "wP5GLRzcTr7X5zzJaxiKN3poepQ5cJQm";
+let weatherURL = "https://dataservice.accuweather.com/currentconditions/v1/";
+let cityURL = "https://dataservice.accuweather.com/locations/v1/cities/search";
 
-  let key = "wP5GLRzcTr7X5zzJaxiKN3poepQ5cJQm";
-  let weatherURL = "https://dataservice.accuweather.com/currentconditions/v1/";
-  let cityURL =
-    "https://dataservice.accuweather.com/locations/v1/cities/search";
+searchUrl =
+  "http://dataservice.accuweather.com/forecasts/v1/daily/5day/49564?apikey=" +
+  key +
+  "&metric=true";
 
-  searchUrl =
-    "http://dataservice.accuweather.com/forecasts/v1/daily/5day/49564?apikey=" +
-    key +
-    "&metric=true";
+fetch(searchUrl)
+  .then(function(responce) {
+    return responce.json();
+  })
+  .then(function(json) {
+    const dailyForecastsArray = json.DailyForecasts;
 
-  fetch(searchUrl)
-    .then(function(responce) {
-      return responce.json();
-    })
-    .then(function(json) {
-      const dailyForecastsArray = json.DailyForecasts;
+    const fiveDayArray = [];
+    for (const day of dailyForecastsArray) {
+      const epochDate = day.EpochDate;
+      const dayIconCode = day.Day.Icon;
+      const dayIconPhrase = day.Day.IconPhrase;
+      const nightIconCode = day.Night.Icon;
+      const nightIconPhrase = day.Night.IconPhrase;
+      const highTemp = day.Temperature.Maximum.Value;
+      const lowTemp = day.Temperature.Minimum.Value;
 
-      const daySummaryArray = [];
-      for (const day of dailyForecastsArray) {
-        const epochDate = day.EpochDate;
-        const dayIconCode = day.Day.Icon;
-        const dayIconPhrase = day.Day.IconPhrase;
-        const nightIconCode = day.Night.Icon;
-        const nightIconPhrase = day.Night.IconPhrase;
-        const highTemp = day.Temperature.Maximum.Value;
-        const lowTemp = day.Temperature.Minimum.Value;
+      const daySummary = {
+        epochDate,
+        dayIconCode,
+        dayIconPhrase,
+        nightIconCode,
+        nightIconPhrase,
+        highTemp,
+        lowTemp
+      };
+      console.log(daySummary);
 
-        const daySummary = {
-          epochDate,
-          dayIconCode,
-          dayIconPhrase,
-          nightIconCode,
-          nightIconPhrase,
-          highTemp,
-          lowTemp
-        };
-        console.log(daySummary);
+      fiveDayArray.push(daySummary);
+    }
 
-        daySummaryArray.push(daySummary);
+    // for each day, render to html
+    const fiveDayContainer = document.getElementById("five_day_container");
+    for (const dayCardData of fiveDayArray) {
+      const dayName = dayCardData.dayName;
+      const daytimeIconCode = dayCardData.dayIconCode;
+      const nighttimeIconCode = dayCardData.nightIconCode;
+      const highTemp = dayCardData.highTemp;
+      const lowTemp = dayCardData.lowTemp;
+      let timeOfDay = "";
+      let date = new Date().getHours();
+      if (date > 12) {
+        timeOfDay = nighttimeIconCode;
+      } else {
+        timeOfDay = daytimeIconCode;
       }
-      return daySummaryArray;
-    });
+      console.log(date);
+      console.log(timeOfDay);
 
-const fiveDayContainer = document.getElementById("five_day_container2");
+      const dayCardHTML = `<div class="day_card">
+                <h2>${dayName}</h2>
 
+                <img class="weather_icon" src="icons/${iconImage}" />
+
+                <p>hi: ${highTemp}°C</p>
+                <p>lo: ${lowTemp}°C</p>
+            </div>`;
+
+      fiveDayContainer.innerHTML += dayCardHTML;
+    }
+  });
